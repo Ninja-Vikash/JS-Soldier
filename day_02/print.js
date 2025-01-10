b1b16b594edd6fd3019653b46a3fe5d5fd466dc1
Vikash Kumar <vikashloharjsr@gmail.com>
2025-01-10 20:00:59 +0530

    day 02 completed

---
+++ day_02/print.js
@@ -0,0 +1,9 @@
+// alert("WHAT IS YOUR NAME?")
+
+console.log("object")
+
+// alert() and prompt() in browser's console
+
+/**
+ *  NOTE: NodeJS environment can't perform client side actions.
+ */
---
+++ day_02/variable.js
@@ -0,0 +1,25 @@
+/**
+ *  JavaScript Comments
+ */
+
+// Single line comment
+
+/**
+ *  Multiline
+ *  comments
+ *  here
+ */
+
+// ---------------------------------------------------------
+
+/**
+ *  JS variables
+ */
+
+var myName = "vikash"
+var myAge = 21
+var isMarried = false
+
+console.log('USER: ',myName)
+console.log('AGE: ',myAge)
+console.log('MARITAL STATUS: ',isMarried)
