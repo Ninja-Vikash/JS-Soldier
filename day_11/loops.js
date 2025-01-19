d32f211df383c1d787b6bda3c2834fcbd101bd23
Vikash Kumar <vikashloharjsr@gmail.com>
2025-01-19 19:47:36 +0530

    day11 completed

---
+++ day_11/loops.js
@@ -0,0 +1,45 @@
+/**
+ *  Loops in Js
+ */
+
+// There are three Major types of loop
+
+// while loop
+
+// If you are well known of ending condition when to stop while loop is great
+
+let i = 0
+
+while(i < 5){
+    console.log(`I'm a JS Soldier!`)
+}
+
+// console.log("Finished!")
+
+/**
+ *  Note:
+ *  1. First declare the initializer
+ *  2. Don't forget to update the initializer value inside the loop otherwise the program will become an infinte loop
+ */
+
+// do-while loop
+
+// It will execute the program first then checks for the condition.
+// Ultimately it will run once.
+
+// let i = 0
+
+do {
+    console.log(`I'm a JS Soldier!`)
+    i++;
+}
+while (i > 5)
+
+console.log("Finished!")
+
+
+console.log(`I'm a JS Soldier`)
+console.log(`I'm a JS Soldier`)
+console.log(`I'm a JS Soldier`)
+console.log(`I'm a JS Soldier`)
+console.log(`I'm a JS Soldier`)
